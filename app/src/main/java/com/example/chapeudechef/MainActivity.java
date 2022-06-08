548707d3afed53758811baea344226e546ff1e55
Matheus Gobhardt Martins <matheusmartins3191@gmail.com>
2022-06-08 11:09:44 -0300

    Versão 0.1 - Ajustada vibração dos botões existentes e iniciada construção das receitas em si.

--- app/src/main/java/com/example/chapeudechef/MainActivity.java
+++ app/src/main/java/com/example/chapeudechef/MainActivity.java
@@ -2,12 +2,11 @@ package com.example.chapeudechef;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.VibrationAttributes;
-import android.os.VibrationEffect;
+import android.os.*;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import com.example.chapeudechef.receitas.ReceitaTortaDeBanana;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,11 +35,253 @@ public class MainActivity extends AppCompatActivity {
         Button btnTortaDeFrango = findViewById(R.id.btnTortaDeFrango);
         Button btnTortaDeLimao = findViewById(R.id.btnTortaDeLimao);
 
+        final Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        btnTortaDeBanana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+
+                Intent intent = new Intent (MainActivity.this, ReceitaTortaDeBanana.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btnTortaDeFrango.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
+        btnStrogonoffDeCarne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
+        btnSopaDeLegumes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
+        btnPureDeBatatas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
+        btnPaoDeQueijo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
+        btnPaoCaseiro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
+        btnPanquecaComFrango.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
+        btnOvosMexidos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
+        btnOvosFritos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
+        btnMassaBolonhesa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
         btnTortaDeLimao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Ingredientes.class);
-                startActivity(intent);
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
+        btnBrusquetas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
+        btnBoloDeCarne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
+        btnFeijoada.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
+        btnLasanhaBolonhesa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
+        btnLasanhaBolonhesa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
+        btnMassaAoAlhoEOlho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
+        btnMassaAoAlhoEOlho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
+        btnMassaCarbonara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
+        btnMassaCarbonara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
+            }
+        });
+        btnMousseDeChocolate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final VibrationEffect vibrationEffect1;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrationEffect1 = VibrationEffect.createOneShot(65, VibrationEffect.DEFAULT_AMPLITUDE);
+                    vibrator.cancel();
+                    vibrator.vibrate(vibrationEffect1);
+                }
             }
         });
 
---
+++ app/src/main/java/com/example/chapeudechef/receitas/ReceitaTortaDeBanana.java
@@ -0,0 +1,14 @@
+package com.example.chapeudechef.receitas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.chapeudechef.R;
+
+public class ReceitaTortaDeBanana extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receita_torta_de_banana);
+    }
+}
